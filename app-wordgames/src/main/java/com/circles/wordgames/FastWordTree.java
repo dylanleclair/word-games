84e10ff2c01bceea3cd5fc54baf9a497546b70a8
Dylan Leclair <dylan.leclair1@ucalgary.ca>
2020-10-07 21:00:03 -0600

    Brought FastNode inline with FastWordTree

--- app-wordgames/src/main/java/com/circles/wordgames/FastWordTree.java
+++ app-wordgames/src/main/java/com/circles/wordgames/FastWordTree.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class FastWordTree extends WordTree {
 
@@ -44,5 +45,36 @@ public class FastWordTree extends WordTree {
     
     }
 
+
+    class FastNode extends Node implements INode {
+
+        protected HashMap<Character, FastNode> letters;
+    
+        public FastNode (FastNode parent, char value)   {
+            super(parent,value);
+            letters = new HashMap<Character, FastNode>();
+        }
+    
+        @Override
+        public FastNode addChild (Character value) {
+            FastNode n = new FastNode(this,value);
+            letters.put(value,n);
+            return n;
+        }
+    
+        public boolean hasChild (char value) {
+            if (letters.containsKey(Character.valueOf(value))) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    
+        @Override
+        public FastNode getChild (Character value) {
+            return letters.get(Character.valueOf(value));
+        }
+    
+    }
 
 }
