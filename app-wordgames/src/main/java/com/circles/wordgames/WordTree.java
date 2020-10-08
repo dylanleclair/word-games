e95a49ee8fb9ef450442dedcbf448461497f2d5d
Dylan Leclair <dylan.leclair1@ucalgary.ca>
2020-10-07 21:03:38 -0600

    Brought Node inline with WordTree

--- app-wordgames/src/main/java/com/circles/wordgames/WordTree.java
+++ app-wordgames/src/main/java/com/circles/wordgames/WordTree.java
@@ -297,4 +297,48 @@ public abstract class WordTree implements IWordTree {
     }
 
 
+    abstract class Node  { 
+    
+        private INode parent;
+        private Character value;
+        private boolean accepting;
+    
+        public Node () {
+    
+        }
+    
+        public void setParent (INode parent) {
+            this.parent = parent;
+        }
+        public INode getParent () {
+            return parent;
+        }
+    
+        public void setAccepting (boolean accepting) {
+            this.accepting = accepting;
+        }
+        public boolean getAccepting () {
+            return accepting;
+        }
+    
+        public void setValue (Character value) {
+            this.value = value;
+        }
+        public Character getValue () {
+            return value;
+        }
+    
+    
+        public Node (INode parent, Character value) {
+            this.parent = parent;
+            this.value = value;
+            accepting = false;
+        }
+    
+    
+    }
+    
+
 }
+
+
