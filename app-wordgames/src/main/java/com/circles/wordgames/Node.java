78a54fd640eaca5070ff1a683e99bdeb16675eb2
Dylan Leclair <dylan.leclair1@ucalgary.ca>
2020-10-07 20:00:04 -0600

    Added INode interface
    Implemented getters and setters for fields in Node

--- app-wordgames/src/main/java/com/circles/wordgames/Node.java
+++ app-wordgames/src/main/java/com/circles/wordgames/Node.java
@@ -1,16 +1,38 @@
 package com.circles.wordgames;
 
-public class Node {
+public abstract class Node  { 
     
-    protected Node parent;
-    protected Character value;
-    protected boolean accepting;
+    private INode parent;
+    private Character value;
+    private boolean accepting;
 
     public Node () {
 
     }
 
-    public Node (Node parent, Character value) {
+    void setParent (INode parent) {
+        this.parent = parent;
+    }
+    INode getParent () {
+        return parent;
+    }
+
+    void setAccepting (boolean accepting) {
+        this.accepting = accepting;
+    }
+    boolean getAccepting () {
+        return accepting;
+    }
+
+    void setValue (Character value) {
+        this.value = value;
+    }
+    Character getValue () {
+        return value;
+    }
+
+
+    public Node (INode parent, Character value) {
         this.parent = parent;
         this.value = value;
         accepting = false;
